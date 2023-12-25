3669b811d7930aa69c2ec2a40d97441cecce0161
xinwu.liu <xinwu.liu@corp.to8to.com>
2023-12-25 10:53:40 +0800

    feat: 格式化

--- src/components/problem-in-arithmetic/index.tsx
+++ src/components/problem-in-arithmetic/index.tsx
@@ -3,11 +3,11 @@ import { ReactElement } from "react";
 import './index.scss'
 
 export default function ProblemInArithmetic(): ReactElement {
-    return <View className="problem-in-arithmetic flex_row justify-content_center">
+    return <View className='problem-in-arithmetic flex_row justify-content_center'>
         <Text>2</Text>
-        <Text className="operator">+</Text>
+        <Text className='operator'>+</Text>
         <Text>3</Text>
-        <Text className="operator">=</Text>
-        <Input className="result"></Input>
+        <Text className='operator'>=</Text>
+        <Input className='result'></Input>
     </View>
 }
